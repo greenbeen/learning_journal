3d83087712391025aae86afa7f706b387e356aa9
greenbeen <kylehart99@yahoo.com>
2015-01-20 21:45:26 -0800

    sort of initial commit

---
+++ learning_journal/views.py
@@ -0,0 +1,74 @@
+from pyramid.httpexceptions import HTTPFound, HTTPNotFound
+from pyramid.view import view_config
+
+from pyramid.security import forget, remember, authenticated_userid
+
+from .models import (
+    DBSession,
+    MyModel,
+    Entry,
+    User,
+    )
+
+from .forms import EntryCreateForm, EntryEditForm, LoginForm
+
+
+@view_config(route_name='home', renderer='templates/list.jinja2')
+def index_page(request):
+    entries = Entry.all()
+    form = None
+    if not authenticated_userid(request):
+        form = LoginForm()
+    return {'entries': entries, 'login_form': form}
+
+
+@view_config(route_name='detail', renderer='templates/detail.jinja2')
+def view(request):
+    this_id = request.matchdict.get('id', -1)
+    entry = Entry.by_id(this_id)
+    if not entry:
+        return HTTPNotFound()
+    return {'entry': entry}
+
+
+@view_config(route_name='action', match_param='action=create',
+             renderer='templates/edit.jinja2', permission='create')
+def create(request):
+    entry = Entry()
+    form = EntryCreateForm(request.POST)
+    if request.method == 'POST' and form.validate():
+        form.populate_obj(entry)
+        DBSession.add(entry)
+        return HTTPFound(location=request.route_url('home'))
+    return {'form': form, 'action': request.matchdict.get('action')}
+
+
+@view_config(route_name='action', match_param='action=edit',
+             renderer='templates/edit.jinja2', permission='edit')
+def update(request):
+    id = request.params.get('id', -1)
+    entry = Entry.by_id(id)
+    if not entry:
+        return HTTPNotFound
+    form = EntryEditForm(request.POST, entry)
+    if request.method == 'POST' and form.validate():
+        form.populate_obj(entry)
+        DBSession.add(entry)
+        return HTTPFound(location=request.route_url('detail', id=entry.id))
+    return {'form': form, 'action': request.matchdict.get('action')}
+
+@view_config(route_name='auth', match_param='action=in', renderer='string',
+     request_method='POST')
+def sign_in(request):
+    login_form = None
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+    if login_form and login_form.validate():
+        user = User.by_name(login_form.username.data)
+        if user and user.verify_password(login_form.password.data):
+            headers = remember(request, user.name)
+        else:
+            headers = forget(request)
+    else:
+        headers = forget(request)
+    return HTTPFound(location=request.route_url('home'), headers=headers)
